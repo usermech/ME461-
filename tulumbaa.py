b57401ba325cd9f3a62a9d5d0b51e6df4a9f1b55
usermech <96394892+usermech@users.noreply.github.com>
2022-02-14 20:31:31 +0300

    Update tulumbaa.py

--- tulumbaa.py
+++ tulumbaa.py
@@ -1,11 +1,9 @@
 import numpy as np
-import time
-from queue import PriorityQueue
-greedyLength = 201
+greedyLength = 301
 boxSize = 50
+halfBox = 25
 nCorr = 8
-colorz = {
-        'black':((1,1,1), 0, 13),
+colorz = {'black': ((1, 1, 1), 0, 13),
         'clr100':((225, 1, 1), 100, 1),
         'clr50':((1, 255, 1), 50, 2),
         'clr30':((1, 1, 255), 30, 2),
@@ -29,64 +27,124 @@ class tulumba:
         self.name = userName  # your object will be given a user name, i.e. your group name
         self.maxStep = maxStepSize  # maximum length of the returned path from run()
         self.maxTime = maxTime  # run() is supposed to return before maxTime
-
-    def run(self, img, info):
-        # get time
-        startTime = time.time()
-
-        grid = []
+        self.colorz = clrDictionary
+        self.grid = []
         for i in range(1, nCorr):
-            grid.append([])
+            self.grid.append([])
             for j in range(1, nCorr):
-                for dClr in colorz:
-                    if tuple(img[i * 2 * boxSize - boxSize, j * 2 * boxSize - boxSize, :]) == colorz[dClr][0]:
-                        node = Node(i, j, colorz[dClr][1])
-                        grid[i - 1].append(node)
-
+                node = Node(i, j)
+                self.grid[i - 1].append(node)
+        
+    def run(self, img, info):
         # get info
         self.info = info
         self.myX,self.myY = self.info[self.name][0]
         
-        # get the nodes closer than 201 steps
-        myQueue = PriorityQueue()
+        # get the nodes closer than 401 steps
+        myQueue = [float("inf")]
         target = []
-        for row in grid:
+        array = []
+        for row in self.grid:
             for node in row:
-                xDistance = node.x_dist(self.myX)
-                yDistance = node.y_dist(self.myY)
-                manDistance = node.manDistance()
-                if manDistance < greedyLength:
-                    myQueue.put(node.man-node.points)
-                    target = [node.x,node.y]
-        for x,y in target:
-            array.append(x,self.myY)
-            array.append(x,y)
+                
+                node.x_dist(self.myX)
+                node.y_dist(self.myY)
+                node.manDistance()
+                 
+                if node.man < greedyLength:
+                    for dClr in colorz:
+                        if tuple(img[node.x, node.y, :]) == colorz[dClr][0]:
+                            node.update_points(colorz[dClr][1])
+                        
+                    if node.man-node.points*2 < myQueue[0] and not node.points == 0:
+                        myQueue[0] = node.man-node.points
+                        target = [node.x,node.y]
+                        
+        
+        x,y = target
+        #call create route here
+        routeX,routeY = create_route(self.myX,x,self.myY,y)
+        array.append(routeX)
+        array.append(routeY)
+        array = [ele for ele in array if ele != []]
+        
+        if get_distance(x,self.myX,y,self.myY) < 95:
+            ##make it a function after here and call it
+            self.myX,self.myY = array[-1]
+            myQueue = [float("inf")]
+            target = []
+            for row in self.grid:
+                for node in row:
+                    if node.x == x and node.y == y:     
+                        node.points = 0
+                    else:  
+                        pass  
+                    
+                    node.x_dist(self.myX)
+                    node.y_dist(self.myY)
+                    node.manDistance()
+                    
+                    if node.man < greedyLength:
+                        
+                        if node.man-node.points*2 < myQueue[0] and not node.points == 0:
+                                
+                            myQueue[0] = node.man-node.points
+                            target = [node.x,node.y]
+           
+            x,y = target
+            routeX,routeY = create_route(self.myX,x,self.myY,y)
+            array.append(routeX)
+            array.append(routeY)
+            array = [ele for ele in array if ele != []]
+            
+            
         return array
+
+def create_route(x1,x2,y1,y2):
+    rx = [x1,y1]
+    
+    if x1 >=  x2 + 25:
+        rx = [x2+24,y1]
+        ry = [x2+24,y1]
+    elif x1 <= x2 -25:
+        rx = [x2-24,y1]
+        ry = [x2-24,y1]
+    else:
+        rx =[]
+        ry = [x1,y1]
+           
+    if y1 >=  y2 + 25:
+        ry[1] = y2 + 24
+    elif y1 <= y2 -25:
+        ry[1] = y2 - 24
+    else:
+        ry = []
+    
+
+    return rx,ry
+
+def get_distance(x1,x2,y1,y2):
+    return abs(x1-x2)+abs(y1-y2)
+
 class Node:
-    def __init__(self,row,column,points):
+    def __init__(self,row,column):
         self.row = row
         self.col = column
+        self.x = self.row * 2 * boxSize - halfBox
+        self.y = self.col * 2 * boxSize - halfBox
+        self.points = 0
+        
+    def update_points(self,points):
         self.points = points
-        self.x1 = self.col * 2 * boxSize - boxSize
-        self.y1 = self.row * 2 * boxSize - boxSize
-        self.x2 = self.x1 + boxSize
-        self.y2 = self.y1 + boxSize
 
-        self.neighbors = []
-    def get_pos(self):
-        return self.row,self.col
     def x_dist(self,myX):
-        dX1 = abs(myX - self.x1)
-        dX2 = abs(myX - self.x2)
-        self.x = min(dX1)
-        return self.x
+        self.dX = abs(myX - self.x)
+        
 
     def y_dist(self,myY):
-        dY1 = abs(myY - self.x1)
-        dY2 = abs(myY - self.x2)
-        self.y = min(dY1,dY2)
-        return self.y
+        self.dY = abs(myY - self.y)
+        
 
     def manDistance(self):
-        self.man = self.x + self.y
-        return self.man
+        self.man = self.dX + self.dY
+        
