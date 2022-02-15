21d8da386bb78bc9c08dd2c1b0cefeaef10341cf
usermech <96394892+usermech@users.noreply.github.com>
2022-02-15 12:25:29 +0300

    Update create_route.py

--- create_route.py
+++ create_route.py
@@ -2,19 +2,19 @@ def create_route(x1,x2,y1,y2):
     array =[[],[]]
     
     
-    if x1 >=  x2 + 25:
+    if x1 >=  x2 + 24:
         array[0] = [x2+23,y1]
         array[1] = [x2+23,y1]
-    elif x1 <= x2 -25:
+    elif x1 <= x2 -24:
         array[0] = [x2-23,y1]
         array[1] = [x2-23,y1]
     else:
         array[0] =[]
         array[1] = [x1,y1]
            
-    if y1 >=  y2 + 25:
+    if y1 >=  y2 + 24:
         array[1][1] = y2 + 23
-    elif y1 <= y2 -25:
+    elif y1 <= y2 -24:
         array[1][1] = y2 - 23
     else:
         array[1] = []
