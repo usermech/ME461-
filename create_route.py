8842d082505e3197442fc3890a8dc4e6668fbc79
usermech <96394892+usermech@users.noreply.github.com>
2022-02-15 12:28:47 +0300

    Update create_route.py

--- create_route.py
+++ create_route.py
@@ -1,9 +1,6 @@
 #enter your coordinates as (x1,y1) and center coordinates of target cell as (x2,y2)
-
 def create_route(x1,x2,y1,y2):
-    array =[[],[]]
-    
-    
+    array =[[],[]]      
     if x1 >=  x2 + 24:
         array[0] = [x2+23,y1]
         array[1] = [x2+23,y1]
@@ -13,14 +10,12 @@ def create_route(x1,x2,y1,y2):
     else:
         array[0] =[]
         array[1] = [x1,y1]
-           
+    
     if y1 >=  y2 + 24:
         array[1][1] = y2 + 23
     elif y1 <= y2 -24:
         array[1][1] = y2 - 23
     else:
         array[1] = []
-    
     array = [ele for ele in array if ele != []]
-
     return array
