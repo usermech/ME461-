5af0fe49312bf3499a8ffa0f40840a68a5c5cca1
usermech <96394892+usermech@users.noreply.github.com>
2022-02-15 12:24:26 +0300

    Add files via upload

---
+++ create_route.py
@@ -0,0 +1,24 @@
+def create_route(x1,x2,y1,y2):
+    array =[[],[]]
+    
+    
+    if x1 >=  x2 + 25:
+        array[0] = [x2+23,y1]
+        array[1] = [x2+23,y1]
+    elif x1 <= x2 -25:
+        array[0] = [x2-23,y1]
+        array[1] = [x2-23,y1]
+    else:
+        array[0] =[]
+        array[1] = [x1,y1]
+           
+    if y1 >=  y2 + 25:
+        array[1][1] = y2 + 23
+    elif y1 <= y2 -25:
+        array[1][1] = y2 - 23
+    else:
+        array[1] = []
+    
+    array = [ele for ele in array if ele != []]
+
+    return array
